1c27b4b795865e2000de8202c99875df628ee44a
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-09-16 09:43:55 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MarkdownEditor from "./components/MarkdownEditor";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="app">
+      <MarkdownEditor />
+    </div>
+  );
+}
+
+export default App;
